31d0405464969c0d6eca9a3d8a3a9cf817b77401
yinsigan <ganweiliang886@gmail.com>
2014-11-11 18:24:50 +0800

    change picture upload controller

--- app/controllers/ckeditor/pictures_controller.rb
+++ app/controllers/ckeditor/pictures_controller.rb
@@ -1,5 +1,5 @@
 class Ckeditor::PicturesController < Ckeditor::ApplicationController
-
+  skip_before_action :verify_authenticity_token
   def index
     @pictures = Ckeditor.picture_adapter.find_all(ckeditor_pictures_scope)
     @pictures = Ckeditor::Paginatable.new(@pictures).page(params[:page])
